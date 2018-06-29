355b71fb9ddf232d831bbc574f0852ad64c59b2d
Shi Huang <742969055@qq.com>
2018-06-29 23:31:50 +0800

    增加翻页功能

--- pages/shelf/shelf.js
+++ pages/shelf/shelf.js
@@ -6,37 +6,69 @@ import env from '../../utils/env.js'
 
 Page({
   data: {
-    bookList:null,
+    bookList:[],
     bookNameSearch: "",
     bookType: "",
     bookStatus: "",
     env: env,
-    showDialog: false
+    showDialog: false,
+    nextNum: 0,//下一页开始序号
+    isLastPage: false,//是否为最后一页
+    loading: false//是否正在加载
   },
   onLoad() {
     // this.searchBookList()
   },
   onShow() {
+    this.resetBookList()
+    // this.searchBookList()
+  },
+  showMore(){
+    // console.info("111");
+    if(!this.data.isLastPage) {
+      this.searchBookList()
+    }
+  },
+  resetBookList() {
+    this.setData({
+      bookList: [],
+      nextNum: 0,
+      isLastPage: false
+    })
     this.searchBookList()
   },
   searchBookList() {
+    this.setData({
+      loading: true //正在加载
+    })
     return util.post("CustQueryBookList", {
       openid: app.globalData.openid,
       bookName: this.data.bookNameSearch,
       bookType: this.data.bookType,
-      bookStatus: this.data.bookStatus
+      bookStatus: this.data.bookStatus,
+      num: this.data.nextNum
     }, null).then(res => {
       var data = res.result
       for(var book of data.bookList) {
         book.bookImage = env.imgurl + book.bookId + '.png'
       }
+      
+      let list = this.data.bookList.concat(data.bookList)
+      // console.info(list)
       this.setData({
-        bookList: data.bookList
+        bookList: list,
+        nextNum: data.endNum,
+        isLastPage: data.lastPage,
+        loading: false // 加载完毕
       })
+      
       this.toggleDialog(false)
     }).catch(res => {
       console.error("error", res)
       this.toggleDialog(false)
+      this.setData({
+        loading: false // 加载完毕
+      })
     })
   },
   changeBookStatus(e) {

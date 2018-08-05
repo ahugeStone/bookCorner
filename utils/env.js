a1168d4400bcef16ca2f977d2780e942fbf179db
puxuewei <xwpiao@gmail.com>
2018-08-05 21:39:49 +0800

    本地调试
    本地调试

--- utils/env.js
+++ utils/env.js
@@ -5,11 +5,12 @@ const url = [// 只使用第1项环境
 // 'https://cvm.ahuangtongxue.cn/bookCornerprod/v1/',// 新版本rest风格后台-生产
 // 'http://cvm.ahuangtongxue.cn:18080/bookCorner/v1/',// 新版本rest风格后台-测试非https
 // 'http://cvm.ahuangtongxue.cn:18081/bookCorner/v1/',// 新版本rest风格后台-生产非https
-'http://localhost:8080/bookCorner/v1/'// 本地调试环境
+'http://localhost:8081/bookCorner/v1/'// 本地调试环境
 ]
 const imgurl = 'https://cvm.ahuangtongxue.cn/images/' // 图书图片的根路径
 const debug = "false" // 是否跳过腾讯oauth-废弃
-const demo = true //是否使用挡板数据数据
+//const demo = true //是使用挡板数据数据
+const demo = false //否使用挡板数据数据
 const springboot = true // 是否使用springboot-废弃
 
 module.exports = {
